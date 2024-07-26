21ab59fda6bde745e31d4acf1f10ef6f1ec503e7
Dani <162794640+DaniellaBarraza125@users.noreply.github.com>
2024-07-26 17:34:36 +0200

    iniciando proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TournamentsComponent } from './components/tournaments/tournaments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'tournament', component: TournamentsComponent},
+];
