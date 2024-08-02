0b877dc9b5a23b2bc664e3326b9e489d82104dfe
Dani <162794640+DaniellaBarraza125@users.noreply.github.com>
2024-08-02 20:32:00 +0200

    dando orden a createTournament

--- src/app/components/tournaments/tournaments.component.ts
+++ src/app/components/tournaments/tournaments.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CreateTournamentComponent } from "../create-tournament/create-tournament.component";
 
 
 
 @Component({
   selector: 'app-tournaments',
   standalone: true,
-  imports: [],
+  imports: [CreateTournamentComponent],
   templateUrl: './tournaments.component.html',
   styleUrl: './tournaments.component.scss',
   providers: []
