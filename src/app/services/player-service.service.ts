894daee6a64d8c1fe37b66e039573b406523c3b4
Dani <162794640+DaniellaBarraza125@users.noreply.github.com>
2024-08-04 13:39:44 +0200

    player service

--- src/app/services/player-service.service.ts
+++ src/app/services/player-service.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { PlayerInterface } from '../interfaces/player-interface';
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerServiceService {
+  private apiUrl = 'http://localhost:8080/players';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createPlayer(player: PlayerInterface): Observable<any> {
+    return this.http.post(this.apiUrl, player);
+  }
 }
